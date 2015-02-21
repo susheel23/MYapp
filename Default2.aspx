24f9f8126306b66207d38e5bf6efc477db0324e5
unknown <sharma.susheel4@gmail.com>
2015-02-21 19:43:03 +0530

    commit all

---
+++ Default2.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage3.master" AutoEventWireup="true" CodeFile="Default2.aspx.cs" Inherits="Default2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    
+    <p>
+        Name&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Address
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="Button1" runat="server" Text="save" OnClick="Button1_Click" />
+    </p>
+    <p>
+        &nbsp;</p>
+      
+</asp:Content>
+
---
+++ Default2.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Default2 : System.Web.UI.Page
+{
+
+    SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+
+    SqlCommand cmd = new SqlCommand();
+        
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        cmd = new SqlCommand("insert into mytbl values('"+TextBox1.Text+"','"+TextBox2.Text+"')", sqlcon);
+        sqlcon.Open();
+        cmd.ExecuteNonQuery();
+        sqlcon.Close();
+
+        Response.Write("done");
+
+    }
+}
